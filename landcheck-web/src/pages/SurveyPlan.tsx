acda73cf119318b0c33ed41a33a00fb65653a7ca
Samuelyohanna9 <samuelyohanna9@gmail.com>
2026-02-22 15:41:06 +0100

    update

--- landcheck-web/src/pages/SurveyPlan.tsx
+++ landcheck-web/src/pages/SurveyPlan.tsx
@@ -943,6 +943,29 @@ export default function SurveyPlan() {
                     </a>
                   </div>
 
+                  {/* Shapefile ZIP */}
+                  <div className="export-card">
+                    <div className="export-icon topo">
+                      <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
+                        <path d="M3 6l6-3 6 3 6-3v15l-6 3-6-3-6 3V6z" />
+                        <path d="M9 3v15M15 6v15" />
+                      </svg>
+                    </div>
+                    <div className="export-info">
+                      <h4>Shapefile (ZIP)</h4>
+                      <p>GIS boundary export for ArcGIS/QGIS</p>
+                    </div>
+                    <a
+                      href={`${BACKEND}/plots/${plotId}/survey-plan/shapefile`}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="download-btn"
+                    >
+                      <svg viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
+                      <span>Download ZIP</span>
+                    </a>
+                  </div>
+
                   {/* Topo Map PDF */}
                   <div className="export-card">
                     <div className="export-icon topo">
