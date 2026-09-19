8ddf83ca945a358efe9dafa9b024260866438ee0
Samuelyohanna9 <samuelyohanna9@gmail.com>
2026-09-20 00:00:43 +0200

    lc work ui upgrade

--- landcheck-web/src/components/green-work/GreenWorkMapPanel.tsx
+++ landcheck-web/src/components/green-work/GreenWorkMapPanel.tsx
@@ -166,7 +166,7 @@ export default function GreenWorkMapPanel(props: GreenWorkMapPanelProps) {
               drawMode={mapAreaDrawMode ? "polygon" : "point"}
               drawActive={mapAreaDrawMode}
               onPolygonChange={mapAreaDrawMode ? (geometry: any) => setNewOrderAreaGeometry(geometry) : undefined}
-              minHeight={mapAreaDrawMode ? 520 : 500}
+              minHeight={mapAreaDrawMode ? 520 : 360}
               onTreeInspect={(detail: any) => {
                 setInspectedTree(detail);
                 if (detail) setMenuOpen(false);
